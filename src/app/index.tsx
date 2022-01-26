43edaf51811e4f9d9dbaa814dee5ce94f083052c
Nguyen Le Quang <quangkeu95@gmail.com>
2022-01-26 14:01:03 +0700

    disable some page on production

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,8 +10,7 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
-// import { Land, Marketplace, Metronion, Staking } from './pages/App/Loadable';
-import { Metronion, Inventory, MetronionInfo } from './pages/App/Loadable';
+// import { Metronion, Inventory, MetronionInfo } from './pages/App/Loadable';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
 import FavIcon from 'src/app/assets/favicon.png';
@@ -57,14 +56,16 @@ export function App() {
       </Helmet>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/metronion" element={<Metronion />} />
+        {/* <Route path="/metronion" element={<Metronion />} /> */}
+        <Route path="/metronion" element={<ComingSoon />} />
         <Route path="/marketplace" element={<ComingSoon />} />
         <Route path="/staking" element={<ComingSoon />} />
         <Route path="/land" element={<ComingSoon />} />
         <Route path="/tokenomic" element={<Tokenomics />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/metronion/:id" element={<MetronionInfo />} />
+        {/* <Route path="/inventory" element={<Inventory />} /> */}
+        <Route path="/inventory" element={<ComingSoon />} />
+        {/* <Route path="/metronion/:id" element={<MetronionInfo />} /> */}
 
         <Route element={<NotFoundPage />} />
       </Routes>
