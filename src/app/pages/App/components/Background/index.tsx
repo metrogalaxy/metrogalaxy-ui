a461232da2cfbbf1b598f96e486cd68e9a6e5cc2
Nguyen Le Quang <quangkeu95@gmail.com>
2022-03-04 19:10:54 +0700

    Update background image

--- src/app/pages/App/components/Background/index.tsx
+++ src/app/pages/App/components/Background/index.tsx
@@ -1,41 +1,12 @@
-import styled from 'styled-components/macro';
-import BackgroundImg from 'src/app/assets/app/background.webp';
 import { LoadingSpinner } from 'src/app/components/Loading';
-
-export const Background = styled.div`
-  background-image: url(${BackgroundImg});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-attachment: fixed;
-  position: absolute;
-  z-index: -3;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: max(100%, 100vh);
-`;
+import { Box, Center } from '@chakra-ui/react';
 
 export function LoadingBackground() {
   return (
-    <Wrapper>
-      <Background />
-      <div className="loading-bg-fullscreen">
+    <Box width="100%" height="100vh" bgColor="pageLayoutBg">
+      <Center height="100vh">
         <LoadingSpinner />
-      </div>
-    </Wrapper>
+      </Center>
+    </Box>
   );
 }
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-
-  .loading-bg-fullscreen {
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
