d842a3199a2cb332a3e1f3957e567934c9d6caad
Nguyen Le Quang <quangkeu95@gmail.com>
2022-03-22 17:18:30 +0700

    Update team and tokenomics

--- src/app/pages/LandingPage/Team.tsx
+++ src/app/pages/LandingPage/Team.tsx
@@ -10,74 +10,72 @@ import {
   Image,
   useMediaQuery,
 } from '@chakra-ui/react';
-import TeamImage1 from './assets/team/dinh_anh_tuyen.webp';
-import TeamImage2 from './assets/team/nguyen_le_quang.webp';
-import TeamImage3 from './assets/team/le_minh_hoang.webp';
-// import TeamImage4 from './assets/team/4.webp';
-import TeamImage5 from './assets/team/nguyen_hoang_giang.webp';
-import TeamImage6 from './assets/team/dang_hoang_long.webp';
-// import TeamImage7 from './assets/team/7.webp';
-import TeamImage8 from './assets/team/nguyen_duy_vu.webp';
-import TeamImage9 from './assets/team/nguyen_thu_linh.webp';
-import TeamImage10 from './assets/team/mac_van_anh.webp';
-// import TeamImage11 from './assets/team/11.webp';
+import CeoImage from './assets/team/dinh_anh_tuyen.webp';
+import CtoImage from './assets/team/nguyen_le_quang.webp';
+import HeadProductImage from './assets/team/le_minh_hoang.webp';
+import CmoImage from './assets/team/trinh_cong_sang.webp';
+import BusinessDevelopmentImage from './assets/team/pham_hoang_minh.webp';
+import GameDevLeadImage from './assets/team/nguyen_hoang_giang.webp';
+import GameServerLeadImage from './assets/team/dang_hoang_long.webp';
+import ArtDirectorImage from './assets/team/nguyen_duy_vu.webp';
+import Artist01Image from './assets/team/nguyen_thu_linh.webp';
+import Artist02Image from './assets/team/mac_van_anh.webp';
 import LinkedinImage from './assets/team/linkedin.svg';
 
 const team = [
   {
-    avatar: TeamImage1,
+    avatar: CeoImage,
     position: 'CEO, Co-Founder',
     name: 'Tuyen Dinh Anh',
     linkedIn: 'https://www.linkedin.com/in/tuyen-dinh-anh-3a033611b/',
-    description: `Growth marketer with experience in business development. Kyber Network Alumni.`,
+    description: `Marketing and Business Development specialist with 5 years of experience in growth hacking and product management. 2 years of experience in growing blockchain products. Kyber Network & Krystal DeFi Alumni.`,
   },
   {
-    avatar: TeamImage2,
+    avatar: CtoImage,
     position: 'CTO, Co-Founder',
     name: 'Quang Nguyen',
     linkedIn: 'https://www.linkedin.com/in/le-quang-nguyen-a42120135/',
-    description: `Full-stack engineer with 4 years experience in blockchain field. 
+    description: `Full-stack engineer with 6 years experience in software development and blockchain. 
     Dapp builder, Kyber Network Alumni.`,
   },
   {
-    avatar: TeamImage3,
+    avatar: HeadProductImage,
     position: 'Head of Product',
     name: 'Hoang Le Minh',
     linkedIn: 'https://www.linkedin.com/in/ho%C3%A0ng-l%C3%AA-minh-4275b4120/',
     description:
       '10 years experience in game design and project management. Strong skills in product management and game publishing.',
   },
-  // {
-  //   avatar: TeamImage4,
-  //   position: 'CMO',
-  //   name: 'Trang Nguyen',
-  // },
   {
-    avatar: TeamImage5,
+    avatar: CmoImage,
+    position: 'CMO',
+    name: 'Sang Trinh Cong',
+    description:
+      '5 years of experience working in the Crypto market, In addition, I used to do marketing for projects such as Contentos, Huobi VietNam and UTU.',
+  },
+  {
+    avatar: BusinessDevelopmentImage,
+    position: 'Business Development',
+    name: 'Minh Pham Hoang',
+    description:
+      'Enthusiastic young marketer with great passion for gamefi, is now working as a full-time business development.',
+  },
+  {
+    avatar: GameDevLeadImage,
     position: 'Game Developer Lead',
     name: 'Giang Nguyen',
     description:
       'Senior game developer with 6 years experience working with Unity. Have built projects which achieve 10+ millions downloads on AppStore and Google Play Store.',
   },
   {
-    avatar: TeamImage6,
+    avatar: GameServerLeadImage,
     position: 'Game Server Developer',
     name: 'Long Dang Hoang',
     description:
       'Senior game server developer with 7 years experience. Strong skills in backend development and database management.',
   },
-  // {
-  //   avatar: TeamImage6,
-  //   position: 'Fullstack Developer',
-  //   name: 'Duc Nguyen',
-  // },
-  // {
-  //   avatar: TeamImage7,
-  //   position: 'Game Server Developer',
-  //   name: 'Thien Dao Thanh',
-  // },
   {
-    avatar: TeamImage8,
+    avatar: ArtDirectorImage,
     position: 'Art Director',
     name: 'Vu Nguyen Duy',
     linkedIn: 'https://www.linkedin.com/in/nguyen-duy-vu-28b81b149/',
@@ -85,14 +83,14 @@ const team = [
       'An experienced leader in game development with excellent management skills and a strong artistic eye. He has hands-on experience of game projects on e-wallet in Malaysia, in which he has the leading role in managing marketing design, concept art game, and visual design. ',
   },
   {
-    avatar: TeamImage9,
+    avatar: Artist01Image,
     position: 'Game Artist',
     name: 'Linh Nguyen Thu',
     description:
       'She is a passionate artist with her love of game, anime and manga. She started drawing because she likes to see the world in a different angle. She hopes her works encourage people freely explore themselves.',
   },
   {
-    avatar: TeamImage10,
+    avatar: Artist02Image,
     position: 'Game Artist',
     name: 'Anh Mac Van',
     description:
